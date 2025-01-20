21c8a6b5bed52872227f32a9ae0e8a2d44822b4e
kemar <marc@marcarea.com>
2025-01-20 16:38:37 +0100

    Implement `AsyncSelect` for data source versions synchronisation dropdown

--- hat/assets/js/apps/Iaso/components/forms/AsyncSelect.tsx
+++ hat/assets/js/apps/Iaso/components/forms/AsyncSelect.tsx
@@ -119,6 +119,12 @@ export const AsyncSelect: FunctionComponent<Props> = ({
                 let newOptions: any[] = [...values];
                 if (results) {
                     newOptions = [...newOptions, ...results];
+                    // Make the array unique by `value` key.
+                    newOptions = [
+                        ...new Map(
+                            newOptions.map(item => [item['value'], item]),
+                        ).values(),
+                    ];
                 }
                 setOptions(newOptions);
             }
@@ -127,7 +133,7 @@ export const AsyncSelect: FunctionComponent<Props> = ({
             active = false;
         };
     }, [values, inputValue, fetch, minCharBeforeQuery]);
-    const displayedOtpions = useMemo(() => [...options] ?? [], [options]);
+    const displayedOptions = useMemo(() => [...options] ?? [], [options]);
     return (
         <Box>
             <Autocomplete
@@ -150,8 +156,8 @@ export const AsyncSelect: FunctionComponent<Props> = ({
                 loadingText={
                     loadingText ? formatMessage(loadingText) : undefined
                 }
-                options={displayedOtpions}
-                value={values}
+                options={displayedOptions}
+                value={multi ? values : values.length > 0 && values[0]}
                 getOptionLabel={option => option?.label ?? ''}
                 filterOptions={(x: any[]) => x}
                 autoComplete
